f6e10b81000bdd2a65499cfc6387b414d91c240a
Shan-e-Shafiq <shafiqshane6@gmail.com>
2024-05-22 23:12:37 +0500

    Testing 2

--- index.cjs
+++ index.cjs
@@ -6,45 +6,45 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express()
 const port = process.env.PORT || 3000
 
-console.log(stripe)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
-// app.use(cors())
+app.post('/payment-sheet', async (req, res) => {
+   // Use an existing Customer ID if this is a returning customer.
+   const customer = await stripe.customers.create();
+   const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: '2024-04-10' }
+   );
+   const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: 'eur',
+      customer: customer.id,
+      // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+      // is optional because Stripe enables its functionality by default.
+      automatic_payment_methods: {
+         enabled: true,
+      },
+   });
 
-// app.post('/payment-sheet', async (req, res) => {
-//    // Use an existing Customer ID if this is a returning customer.
-//    const customer = await stripe.customers.create();
-//    const ephemeralKey = await stripe.ephemeralKeys.create(
-//       { customer: customer.id },
-//       { apiVersion: '2024-04-10' }
-//    );
-//    const paymentIntent = await stripe.paymentIntents.create({
-//       amount: 1099,
-//       currency: 'eur',
-//       customer: customer.id,
-//       // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-//       // is optional because Stripe enables its functionality by default.
-//       automatic_payment_methods: {
-//          enabled: true,
-//       },
-//    });
+   res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
+   });
+});
 
-//    res.json({
-//       paymentIntent: paymentIntent.client_secret,
-//       ephemeralKey: ephemeralKey.secret,
-//       customer: customer.id,
-//       publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
-//    });
-// });
+app.get('/', (req, res) => {
+   res.status(200).json({
+      "msg": "SERVER IS UP"
+   })
+})
 
-// app.get('/', (req, res) => {
-//    res.status(200).json({ 'msg': "SERVER IS UP" })
-// })
-
-// app.listen(port, () => {
-//    console.log('App is listening on PORT : ' + port)
-// })
+app.listen(port, () => {
+   console.log('Listening on PORT : ' + port)
+})
 
 
 
