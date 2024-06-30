12785bf096a253b0cfb6e3d6e4bc5cc022710d91
Shan-e-Shafiq <shafiqshane6@gmail.com>
2024-06-30 23:40:07 +0500

    Testing3

--- index.cjs
+++ index.cjs
@@ -13,30 +13,38 @@ app.use(cors({
 }))
 
 app.post('/payment-sheet', async (req, res) => {
-   const { amount } = req.body
-   // Use an existing Customer ID if this is a returning customer.
-   const customer = await stripe.customers.create();
-   const ephemeralKey = await stripe.ephemeralKeys.create(
-      { customer: customer.id },
-      { apiVersion: '2024-04-10' }
-   );
-   const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100,
-      currency: 'usd',
-      customer: customer.id,
-      // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-      // is optional because Stripe enables its functionality by default.
-      automatic_payment_methods: {
-         enabled: true,
-      },
-   });
+   try {
+      console.log(req.body)
+      const { amount } = req.body
+      // Use an existing Customer ID if this is a returning customer.
+      const customer = await stripe.customers.create();
+      const ephemeralKey = await stripe.ephemeralKeys.create(
+         { customer: customer.id },
+         { apiVersion: '2024-04-10' }
+      );
+      const paymentIntent = await stripe.paymentIntents.create({
+         amount: amount * 100,
+         currency: 'usd',
+         customer: customer.id,
+         // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+         // is optional because Stripe enables its functionality by default.
+         automatic_payment_methods: {
+            enabled: true,
+         },
+      });
 
-   res.json({
-      paymentIntent: paymentIntent.client_secret,
-      ephemeralKey: ephemeralKey.secret,
-      customer: customer.id,
-      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
-   });
+      res.json({
+         paymentIntent: paymentIntent.client_secret,
+         ephemeralKey: ephemeralKey.secret,
+         customer: customer.id,
+         publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
+      });
+   } catch (error) {
+      res.status(500).json({
+         'Error': error,
+         'Req.body': req.body
+      })
+   }
 });
 
 app.get('/', (req, res) => {
