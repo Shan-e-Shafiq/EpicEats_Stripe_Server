287da628e2a11e9f3025236e6f3bc547d2c36c06
Shan-e-Shafiq <shafiqshane6@gmail.com>
2024-08-02 18:01:44 +0500

    Updates

--- index.cjs
+++ index.cjs
@@ -15,7 +15,6 @@ app.use(cors({
 app.post('/payment-sheet', async (req, res) => {
    try {
       const { amount, customerID } = req.body
-      // Use an existing Customer ID if this is a returning customer.
       const customer = customerID
       const ephemeralKey = await stripe.ephemeralKeys.create(
          { customer: customer },
@@ -25,13 +24,10 @@ app.post('/payment-sheet', async (req, res) => {
          amount: amount,
          currency: 'pkr',
          customer: customer,
-         // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-         // is optional because Stripe enables its functionality by default.
          automatic_payment_methods: {
             enabled: true,
          },
       });
-
       res.json({
          paymentIntent: paymentIntent.client_secret,
          ephemeralKey: ephemeralKey.secret,
